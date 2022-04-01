6d54fdcbdd16e4cf74c52b5dbfca82e62e0f6fac
Lindsey Still <lindseymstill@gmail.com>
2022-04-02 03:14:36 +0900

    added student class

--- school_schedule/student.py
+++ school_schedule/student.py
@@ -1,3 +1,25 @@
 class Student:
-    pass
+    '''
+    Methods:
+    add_class()
+    get_num_classes()
+    summary()
+    '''
+
+    def __init__(self, name, year, courses, school = "Ada Developers Academy"):
+        self.name = name
+        self.year = year
+        self.courses = courses
+        self.school = school
+    
+    def add_class(self, course_name):
+        if course_name:
+            self.courses.append(course_name)
+        return self.courses
+
+    def get_num_classes(self):
+        return len(self.courses)
+    
+    def summary(self):
+        print(f"{self.name} is a {self.year} taking {self.get_num_classes()} classes this semester.")
     
